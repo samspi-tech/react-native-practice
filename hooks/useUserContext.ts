18833d457762d2b09490b6bf50f789fc11d55e5b
samspi-tech <andrew.ryan.r.a@gmail.com>
2026-02-26 22:08:39 +0100

    refactor

--- hooks/useUserContext.ts
+++ hooks/useUserContext.ts
@@ -6,7 +6,7 @@ export const useUserContext = () => {
     const context = useContext(UserContext);
 
     if (!context) {
-        throw new Error('useUser must be used within the UserProvider');
+        throw new Error('useUserContext must be used within the UserProvider');
     }
 
     return context;
