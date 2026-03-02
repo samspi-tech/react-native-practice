889d2deabc4caf6051a110f33064d382dcc7c336
samspi-tech <andrew.ryan.r.a@gmail.com>
2026-03-02 16:24:34 +0100

    setup Appwrite database

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,4 +1,4 @@
-import { Client, Account, Avatars } from 'react-native-appwrite';
+import { Client, Account, Avatars, Databases } from 'react-native-appwrite';
 
 export const client = new Client()
     .setEndpoint(`${process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT}`)
@@ -6,3 +6,4 @@ export const client = new Client()
 
 export const account = new Account(client);
 export const avatars = new Avatars(client);
+export const databases = new Databases(client);
