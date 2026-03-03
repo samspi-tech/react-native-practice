c9aecd6ff793ec69713fd51d875ff5c54ca14d79
samspi-tech <andrew.ryan.r.a@gmail.com>
2026-03-03 22:49:54 +0100

    display books with FlatList

--- app/(dashboard)/books.tsx
+++ app/(dashboard)/books.tsx
@@ -1,14 +1,35 @@
-import { StyleSheet } from 'react-native';
+import { FlatList, Pressable, StyleSheet } from 'react-native';
 
 import ThemedText from '../../components/ThemedText';
 import ThemedView from '../../components/ThemedView';
+import { useBooksContext } from '../../hooks/useBooksContext';
+import { Colors } from '../../constants/Colors';
+import ThemedCard from '../../components/ThemedCard';
 
 const Books = () => {
+    const { books } = useBooksContext();
+
     return (
         <ThemedView isSafeView={true} style={styles.container}>
             <ThemedText isTitle={true} style={styles.heading}>
                 Your Reading List
             </ThemedText>
+
+            <FlatList
+                data={books}
+                keyExtractor={(book) => book.$id}
+                contentContainerStyle={styles.list}
+                renderItem={({ item }) => (
+                    <Pressable>
+                        <ThemedCard style={styles.card}>
+                            <ThemedText style={styles.title}>
+                                {item.title}
+                            </ThemedText>
+                            <ThemedText>Written by {item.author}</ThemedText>
+                        </ThemedCard>
+                    </Pressable>
+                )}
+            />
         </ThemedView>
     );
 };
@@ -25,5 +46,22 @@ const styles = StyleSheet.create({
         fontSize: 18,
         textAlign: 'center',
         marginTop: 40,
+    },
+    list: {
+        marginTop: 40,
+        gap: 10,
+    },
+    card: {
+        width: '90%',
+        marginHorizontal: '5%',
+        padding: 10,
+        paddingLeft: 14,
+        borderLeftColor: Colors.primary,
+        borderLeftWidth: 4,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 10,
     },
 });
